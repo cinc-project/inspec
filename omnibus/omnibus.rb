a169ab3924ce70b1b135301dc8f6ee89da21749c
Lance Albertson <lance@osuosl.org>
2025-09-24 09:36:58 -0700

    Switch to using OSL S3 bucket
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -40,9 +40,10 @@ use_internal_sources ENV.fetch("OMNIBUS_USE_INTERNAL_SOURCES", true)
 use_s3_caching true
 s3_access_key  ENV["AWS_ACCESS_KEY_ID"]
 s3_secret_key  ENV["AWS_SECRET_ACCESS_KEY"]
-s3_bucket      "opscode-omnibus-cache-private"
-s3_acl         "private"
-s3_region      "us-west-2"
+s3_bucket      "cinc-omnibus-cache"
+s3_endpoint    "https://s3.osuosl.org"
+s3_force_path_style true
+
 build_retries 3
 fetcher_retries 3
 fetcher_read_timeout 120

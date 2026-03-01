99a09693a17fb711cd64a2b9a9ac2ff1a3880b4e
Lance Albertson <lance@osuosl.org>
2026-03-01 10:07:42 -0800

    fix: Update libffi to 3.4.7 to fix GCC 14 build failure
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -6,7 +6,7 @@ override "train", version: "v#{train_stable}"
 override "ruby", version: "3.1.7"
 
 override :openssl, version: "3.2.6"
-override :libffi, version: "3.4.6" # Pinning to this version, as 3.4.4 is incompatible with RHEL-10.
+override :libffi, version: "3.4.7" # Pinning to this version, as 3.4.4 is incompatible with RHEL-10.
 override :zlib, version: "1.3.1"
 
 override "ruby-msys2-devkit", version: "3.1.7-1"

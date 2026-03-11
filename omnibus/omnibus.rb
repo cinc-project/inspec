017ead1b57302e5f31f0b1c5284661770df23c35
Lance Albertson <lance@osuosl.org>
2026-03-11 16:09:25 -0700

    Don't use internal sources
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -33,7 +33,7 @@
 use_s3_caching true
 
 # note, this is statically set in the omnibus-buildkite-plugin, you are always going to be forced to use internal sources. If you dont want internal sources, you must enable this to false.
-use_internal_sources ENV.fetch("OMNIBUS_USE_INTERNAL_SOURCES", true)
+use_internal_sources ENV.fetch("OMNIBUS_USE_INTERNAL_SOURCES", false)
 
 # Enable S3 asset caching
 # ------------------------------
